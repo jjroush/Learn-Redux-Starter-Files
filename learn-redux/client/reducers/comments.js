c46ea4868eb2f414327d1c1509cfbea1b0b4b142
jroush <jroush@hy-vee.com>
2018-07-03 13:31:02 -0500

    added add & remove comment

--- learn-redux/client/reducers/comments.js
+++ learn-redux/client/reducers/comments.js
@@ -1,5 +1,34 @@
+function postComments(state = [], action) {
+    switch (action.type) {
+        case 'ADD_COMMENT':
+            //return existing state and new state
+            return [...state, {
+                user: action.author,
+                text: action.comment
+            }]
+        case 'REMOVE_COMMENT':
+            console.log('remove comment');
+            //return new state without deleted comment
+            return [
+                ...state.slice(0,action.i),
+                ...state.slice(action.i + 1)
+            ]
+        default:
+            return state;
+    }
+    return state;
+}
+
 function comments(state = [], action) {
-    console.log(state, action);
+    if(typeof action.postId !== 'undefined') {
+        return {
+            //current state
+            ...state,
+            //overwrite this post with a new one
+            [action.postId]: postComments(state[action.postId], action)
+        }
+    }
+
     return state;
 }
 
--- learn-redux/client/reducers/posts.js
+++ learn-redux/client/reducers/posts.js
@@ -3,8 +3,18 @@
 //2. copy of current state
 
 function posts(state = [], action) {
-    console.log('this post will change');
-    console.log(state, action);
+    switch(action.type) {
+        case 'INCREMENT_LIKES' :
+            console.log('test');
+            const i =action.index;
+            return [
+                ...state.slice(0,i),
+                {...state[i], likes: state[i].likes + 1},
+                ...state.slice(i+1),
+            ]
+        default:
+            return state;
+    }
     return state;
 }
 
